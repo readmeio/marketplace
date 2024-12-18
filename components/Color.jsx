6a37b92ee9a5fb9519268facfcd8fbd31f6a8caf
gkoberger <gkoberger@gmail.com>
2024-12-17 23:08:54 -0500

    proposal for mdx components

---
+++ components/Color.jsx
@@ -0,0 +1,9 @@
+---
+title: Color Component
+version: 4.8
+author: Greg Koberger
+---
+
+export const Color = ({ color }) => <div style={{color}} className="text-xs">I'm colorful!</div>;
+
+<Color color="red">This text is colored!</Color>
